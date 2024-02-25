a19ac9beb41ee59a495110179bfedf612cbaba96
sahithallam5030 <106263746+sahithallam5030@users.noreply.github.com>
2024-02-25 16:03:44 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,7 +1,18 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore,combineReducers} from '@reduxjs/toolkit'
 import userReducer from './slices/userSlice'
+import storage from 'redux-persist/lib/storage'
+import {persistReducer} from 'redux-persist'
+const persistConfig={
+    key:'root',
+    version:1,
+    storage
+}
+
+const reducer=combineReducers({
+    users:userReducer
+})
+
+const newpersistReducer=persistReducer(persistConfig,reducer);
 export const store=configureStore({
-    reducer:{
-        users:userReducer
-    }
+    reducer:newpersistReducer
 })
