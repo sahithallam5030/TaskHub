bbe9c1958c7f5f05526136af339240d7eacc5a9c
sahithallam5030 <106263746+sahithallam5030@users.noreply.github.com>
2024-02-25 16:04:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,18 @@ import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {store} from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {PersistGate} from 'redux-persist/integration/react'
+import {persistStore} from 'redux-persist'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let persistor=persistStore(store);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <Provider store={store}>
+  <PersistGate persistor={persistor}>
     <App />
+  </PersistGate>
     </Provider>
     </BrowserRouter>
   </React.StrictMode>
